6bda7b7c5877e9d3cff0e1bc15e8fa8d25a08de2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 15:51:24 -0700

    changed index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-  phpinfo();
+<head>
 
-?>
+    <title>Homepage CS313</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+    <div class="jumbotron text-center">
+        <h1>This is a test</h1>
+    </div>
+
+</body>
+
+
+</html>
