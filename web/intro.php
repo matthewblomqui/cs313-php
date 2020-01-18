8753246ce979bce7da6f12a7806de70c03248be5
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 23:08:34 -0700

    change

--- web/intro.php
+++ web/intro.php
@@ -26,6 +26,17 @@
             <div class="col">
                 <h3>About Me</h3>
                 <p class="text-left">I am a Student and Tutor and Brigham Young University - Idaho. My Major is Computer Science and I want to be a software developer for Google. I am currently a Junior and still looking for internships. My interests are in machine learning, artificial intelligence, and autonomous robotics.</p>
+                <br>
+                <h3>Skills</h3>
+                <p class="text-left">
+                    <ul>
+                        <li>C++ (Proficient)</li>
+                        <li>Python (Familiar)</li>
+                        <li>Front End (Familiar)</li>
+                        <li>Back End (Familiar)</li>
+                        <li>Git (Familiar)</li>
+                    </ul>
+                </p>
             </div>
 
             <div class="col">
@@ -35,11 +46,11 @@
                 <br>
                 <h5>C++ Tutor</h5>
                 <p class="text-left">- Conducted one-on-one tutoring sessions that helped students increase their understanding with the basics of programming C++, real-world physics, problem-solving, and debugging<br>
-                - Prepared students to learn how to study and become more independent</p>
+                    - Prepared students to learn how to study and become more independent</p>
                 <br>
                 <h5>Data Center Technician</h5>
                 <p class="text-left">- Deployed and maintained Google's advanced Data Center Server and Network Infrastructure by working as a member of a large, coordinated team<br>
-                - Trained co-workers by sharing knowledge and skills to complete procedures properly and efficiently</p>
+                    - Trained co-workers by sharing knowledge and skills to complete procedures properly and efficiently</p>
             </div>
         </div>
 
