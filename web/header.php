d835a379b90329ff97b783d7446d509463802148
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 19:49:18 -0700

    change

---
+++ web/header.php
@@ -0,0 +1,19 @@
+<?php
+echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Intro</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#">Assignments</a>
+            </li>
+        </ul>
+    </nav>'
+?>
--- web/index.php
+++ web/index.php
@@ -11,23 +11,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Intro</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a class="nav-link" href="#">Assignments</a>
-            </li>
-        </ul>
-    </nav>
+    <?php include 'header.php';?>
 
     <div class="container">
         <div class="row">
