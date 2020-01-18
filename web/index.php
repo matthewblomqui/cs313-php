7c6fe19e98cc1f28da942440c7ff750d00a950d3
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 20:38:16 -0700

    change

--- web/index.php
+++ web/index.php
@@ -7,10 +7,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
 
 </head>
 
-<body style="background-image: url('407.svg');">
+<body>
     <?php include 'header.php';?>
 
     <div class="container pt-3">
