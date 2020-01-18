db3c5dc44de77914609101477e3360cbca5ec62d
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 22:57:41 -0700

    change

--- web/intro.php
+++ web/intro.php
@@ -25,20 +25,20 @@
         <div class="row text-center">
             <div class="col">
                 <h3>About Me</h3>
-                <p>I am a Student and Tutor and Brigham Young University - Idaho. My Major is Computer Science and I want to be a software developer for Google. I am currently a Junior and still looking for internships. My interests are in machine learning, artificial intelligence, and autonomous robotics.</p>
+                <p class="text-left">I am a Student and Tutor and Brigham Young University - Idaho. My Major is Computer Science and I want to be a software developer for Google. I am currently a Junior and still looking for internships. My interests are in machine learning, artificial intelligence, and autonomous robotics.</p>
             </div>
 
             <div class="col">
                 <h3>Experience</h3>
                 <h4>IT Support</h4>
-                <p>- I did troubleshooting with a wide variety of computing issues and provided direct software support</p>
+                <p class="text-left">- I did troubleshooting with a wide variety of computing issues and provided direct software support</p>
                 <br>
                 <h4>C++ Tutor</h4>
-                <p>- Conducted one-on-one tutoring sessions that helped students increase their understanding with the basics of programming C++, real-world physics, problem-solving, and debugging
+                <p class="text-left">- Conducted one-on-one tutoring sessions that helped students increase their understanding with the basics of programming C++, real-world physics, problem-solving, and debugging
                 - Prepared students to learn how to study and become more independent</p>
                 <br>
                 <h4>Data Center Technician</h4>
-                <p>- Deployed and maintained Google's advanced Data Center Server and Network Infrastructure by working as a member of a large, coordinated team
+                <p class="text-left">- Deployed and maintained Google's advanced Data Center Server and Network Infrastructure by working as a member of a large, coordinated team
                 - Trained co-workers by sharing knowledge and skills to complete procedures properly and efficiently</p>
             </div>
         </div>
