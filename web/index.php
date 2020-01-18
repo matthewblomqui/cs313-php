9b6e2757319aba9558f9a1ff9ae77ab0b46f4407
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 21:48:20 -0700

    change

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@
 <body class="bg-light">
     <?php include 'header.php';?>
 
-    <div class="container pt-3 pb-6">
+    <div class="container py-6">
         <div class="row">
             <div class="col-md-6">
                 <div class="card" style="width:400px">
