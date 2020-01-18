59a8c5281e0437bf63007fb817427bbbd3d8d53c
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-17 19:33:13 -0700

    change

--- web/footer.php
+++ web/footer.php
@@ -1,5 +1,5 @@
 <?php
-echo '<footer class="py-4 bg-dark text-white-50 fixed-bottom">'.
+echo '<footer class="py-4 bg-dark text-white-50">'.
      '<div class="container text-center">'.
      '<small>Matthew Blomquist - Computer Science - CS313 Web Engineering II - '.date("Y").
      '</small></div></footer>';
