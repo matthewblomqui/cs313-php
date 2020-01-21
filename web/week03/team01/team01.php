d32ae0cda7a8de1240d3ecd900094aa889df7f66
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-21 16:27:40 -0700

    change

--- web/week03/team01/team01.php
+++ web/week03/team01/team01.php
@@ -9,25 +9,38 @@
 </head>
 
 <body>
-<?php
-echo '
+
     <form action="tuesdayPost.php" method="post">
 
+        <!-- 'name' input -->
         <div class="form-group">
             <label for="name">Name:</label>
             <input type="text" name="textForPHP" class="form-control" placeholder="Enter name" id="name">
         </div>
 
+        <!-- 'email' input -->
         <div class="form-group">
             <label for="email">Email address:</label>
             <input type="email" name="emailForPHP" class="form-control" placeholder="Enter email" id="email">
         </div>
 
+        <!-- 'password' input -->
         <div class="form-group">
             <label for="pwd">Password:</label>
             <input type="password" name="passwordForPHP" class="form-control" placeholder="Enter password" id="pwd">
         </div>
 
+        <!-- Radio buttons -->
+        <?php
+            $majors = array("Computer Science, CS", "Web Design and Development, WDD", "Computer Information Technology, CIT", "Computer Engineering, CE");
+            foreach ($majors as $key => $value) {
+               echo "<div class=\"form-check-inline\">
+                        <label class=\"form-check-label\">
+                           <input type=\"radio\" class=\"form-check-input\" name=\"optradio\" value=\"$value\">$value
+                        </label>
+                     </div>";
+            }
+        ?>
         <div class="form-check-inline">
             <label class="form-check-label">
                 <input type="radio" class="form-check-input" name="optradio" value="Computer Science">Computer Science
@@ -42,37 +55,39 @@ echo '
         
         <div class="form-check-inline">
             <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="optradio" value="Computer information Technology">Computer information Technology
+                <input type="radio" class="form-check-input" name="optradio" value="Computer Information Technology">Computer information Technology
             </label>
         </div>
-
+    
         <div class="form-check-inline">
             <label class="form-check-label">
                 <input type="radio" class="form-check-input" name="optradio" value="Computer Engineering">Computer Engineering
             </label>
         </div>
+        <!-- End of radio button -->
 
+        <!-- Textarea/ comments -->
         <div class="form-check-inline">
             <label class="form-check-label">
                 <input type="textarea" class="form-check-input" name="textbox">
             </label>
         </div>
 
+        <!-- checkboxes -->
         <div class="form-check-inline">
             <label class="form-check-label">
-                <input type="checkbox" id="country1" name="countries[]" value="North America"> North America<br>
-                <input type="checkbox" id="country2" name="countries[]" value="South America"> South America<br>
-                <input type="checkbox" id="country3" name="countries[]" value="Europe"> Europe<br>
-                <input type="checkbox" id="country4" name="countries[]" value="Asia"> Asia<br>
-                <input type="checkbox" id="country5" name="countries[]" value="Australia"> Australia<br>
-                <input type="checkbox" id="country6" name="countries[]" value="Africa"> Africa<br>
-                <input type="checkbox" id="country7" name="countries[]" value="Antarctica"> Antarctica<br>
+                <input type="checkbox" id="country1" name="countries[]" value="NA"> North America<br>
+                <input type="checkbox" id="country2" name="countries[]" value="SA"> South America<br>
+                <input type="checkbox" id="country3" name="countries[]" value="E"> Europe<br>
+                <input type="checkbox" id="country4" name="countries[]" value="AS"> Asia<br>
+                <input type="checkbox" id="country5" name="countries[]" value="AU"> Australia<br>
+                <input type="checkbox" id="country6" name="countries[]" value="AF"> Africa<br>
+                <input type="checkbox" id="country7" name="countries[]" value="AN"> Antarctica<br>
             </label>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
-    </form>'
-    ?>
+    </form>
 </body>
 
 </html>
