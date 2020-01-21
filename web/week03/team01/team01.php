c8e1364f266f1d42773ff2acc21d54e5b33289f8
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-21 15:33:17 -0700

    team act. for week03

---
+++ web/week03/team01/team01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+</head>
+<body>
+   <form action="" method="post">
+      <div class="form-group">
+         <label for="email">Email address:</label>
+         <input type="email" class="form-control" placeholder="Enter email" id="email">
+      </div>
+      <div class="form-group">
+         <label for="pwd">Password:</label>
+         <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+      </div>
+      <div class="form-group form-check">
+         <label class="form-check-label">
+            <input class="form-check-input" type="checkbox"> Remember me
+         </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+   </form>
+</body>
+</html>
---
+++ web/week03/team01/tuesdayPost.php
@@ -0,0 +1,8 @@
+<?php
+   // retrieve POST data
+   $email = htmlspecialchars($_POST["emailForPHP"]);
+   $password = htmlspecialchars($_POST["passwordForPHP"]);
+
+   // do something with the data
+   echo "Here is your email $email, and your password is $password... very unsecure";
+?>
