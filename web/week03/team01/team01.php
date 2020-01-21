ec5c2baea347d07a12d7b17c2287cabebe1e79cf
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-21 15:48:32 -0700

    change

--- web/week03/team01/team01.php
+++ web/week03/team01/team01.php
@@ -1,27 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
+
 <body>
-   <form action="tuesdayPost.php" method="post">
-      <div class="form-group">
-         <label for="email">Email address:</label>
-         <input type="email" name="emailForPHP" class="form-control" placeholder="Enter email" id="email">
-      </div>
-      <div class="form-group">
-         <label for="pwd">Password:</label>
-         <input type="password" name="passwordForPHP" class="form-control" placeholder="Enter password" id="pwd">
-      </div>
-      <div class="form-group form-check">
-         <label class="form-check-label">
-            <input class="form-check-input" type="checkbox"> Remember me
-         </label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-   </form>
+    <form action="tuesdayPost.php" method="post">
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" name="emailForPHP" class="form-control" placeholder="Enter email" id="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" name="passwordForPHP" class="form-control" placeholder="Enter password" id="pwd">
+        </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="optradio">Computer Science
+            </label>
+        </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="optradio">Web Design and Development
+            </label>
+        </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="optradio">Computer information Technology
+            </label>
+        </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="optradio">Computer Engineering
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </body>
+
 </html>
