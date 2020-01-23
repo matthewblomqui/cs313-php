6b1d4030335ee6866751fc5e7a19ab99caf2f738
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-23 16:18:57 -0700

    change

--- web/week03/thursdayClassTemp/thursdaySession2.php
+++ web/week03/thursdayClassTemp/thursdaySession2.php
@@ -3,8 +3,8 @@
    session_start();
 
    // save session variables into local variables
-   $c = $_SESSION['favcolor'];
-   $a = $_SESSION['favanimal'];
+   $c = $_SESSION["favcolor"];
+   $a = $_SESSION["favanimal"];
 ?>
 <h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?></h1>
 
