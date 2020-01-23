a1db996f609f49341fddaf6bb20bdce5088d2354
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-23 16:22:37 -0700

    change

--- web/week03/thursdayClassTemp/thursdaySession1.php
+++ web/week03/thursdayClassTemp/thursdaySession1.php
@@ -9,9 +9,8 @@
          //unset($_SESSION['pictureUrl']);
 
          // Set session variables
-         $_SESSION["favcolor"] = "green";
+         $_SESSION["favcolor"] = "blue";
          $_SESSION["favanimal"] = "snow leopard";
-         $_SESSION["favgame"];
 
          // echo that variables have been set
          echo "Session variables have been set.";
@@ -20,7 +19,7 @@
       <a href="thursdaySession2.php">Check the variables on another page</a>
 
       <h3>Just for kicks, let's try this with a form</h3>
-      <form action="" method="post">
+      <form action="thursdaySession2.php" method="post">
          <input type="text" name="picture">
          <input type="submit" name="Submit" value="Submit!">
       </form>
