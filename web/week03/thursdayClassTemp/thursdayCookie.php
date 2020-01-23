fc7cbcf9cd0f0109db748c48f9e66284136ff81f
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-23 15:55:23 -0700

    change

--- web/week03/thursdayClassTemp/thursdayCookie.php
+++ web/week03/thursdayClassTemp/thursdayCookie.php
@@ -1,3 +1,3 @@
 <h3>Display all cookies</h3>
 <?php print_r($_COOKIE); // code to display all cookies ?>
-<h1><?php // use the single cookie ?></h1>
+<h1><?php echo $_COOKIE['user'];// use the single cookie ?></h1>
