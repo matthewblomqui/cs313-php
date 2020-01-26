2aa72f569719c1b3feaaecc532c86d446a0b3ee1
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-25 21:43:19 -0700

    change

--- web/week03/shopping_cart/browse_shop.php
+++ web/week03/shopping_cart/browse_shop.php
@@ -15,20 +15,13 @@
    <body>
 
       <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-         <a class="navbar-brand" href="#">
+         <a class="navbar-brand" href="browse_shop.php">
             <img src="pokemart_logo_small.jpg" alt="Logo" style="width:40px;">
          </a>
          <h3 class="text-light">Pokemart Shop</h3>
-         <div class="collapse navbar-collapse" id="navb">
-            <ul class="navbar-nav mr-auto">
-               <li class="nav-item">
-               <a class="nav-link" href="#">Cart</a>
-               </li>
-            </ul>
-            <a class="" href="view_cart.php">
-               <img src="cart.png" alt="Logo" style="background: white; border-radius: 50%;width:60px;">
-            </a>
-         </div>
+         <a class="" href="view_cart.php">
+            <img src="cart.png" alt="Logo" style="background: white; border-radius: 50%;width:60px;">
+         </a>
       </nav>
 
       <div class="row">
--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -33,7 +33,7 @@
       <div class="row">
          <div class="col-2"></div>
          <div class="col">
-            <h2 class="text-primary">Cart Details</h2>
+            <h2 class="text-primary text-center">Cart Details</h2>
             <?php
                print_r($_SESSION);
             ?>
