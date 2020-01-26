7cf543aebee4aaf422db1fa23dcf59466e656fc3
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-25 22:47:49 -0700

    change

--- web/week03/shopping_cart/browse_shop.php
+++ web/week03/shopping_cart/browse_shop.php
@@ -88,8 +88,8 @@
                <div class="row h-24"> <!-- Single item -->
                   <div class="triangle-left col-2"></div>
                   <div class="col"></div>
-                     <input type="submit" name="Submit" value="Add to Cart" class="btn btn-primary float-right">
-                     <input type="reset" name="clear" value="Clear Cart" class="btn btn-warning float-right">
+                     <input type="submit" name="Submit" value="Add to Cart" class="btn btn-primary">
+                     <input type="reset" name="clear" value="Clear Cart" class="btn btn-warning">
                   </div>
                   <div class="triangle-right col-2"></div>
                </div>
