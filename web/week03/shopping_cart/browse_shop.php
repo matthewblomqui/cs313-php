a15f57a5083109ffc77147241b8d057c2f92402f
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-25 19:03:54 -0700

    change

--- web/week03/shopping_cart/browse_shop.php
+++ web/week03/shopping_cart/browse_shop.php
@@ -5,12 +5,17 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title>Pokemart Shop</title>
-      <meta name="description" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
       <link rel="stylesheet" href="shop.css">
    </head>
    <body>
       <h3>content</h3>
+
+      <div class="container">
+         Item template...
+      </div>
       
+      <?php include 'https://matthewblomqui.herokuapp.com/footer.php';?>
    </body>
 </html>
