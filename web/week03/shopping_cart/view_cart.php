046145b9f682b670a1f1bc1d3cbc64123463fcfe
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 00:00:58 -0700

    change

--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -18,7 +18,9 @@
       <div class="row">
          <div class="col-2"></div>
          <div class="col pt-3 pokeDark">
+            <hr>
             <h2 class="text-primary text-center">Cart Details</h2>
+            <hr>
             <div class="row">
                <?php
                   print_r($_SESSION);
