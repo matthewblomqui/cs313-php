14735e87a21ee71fba88e64baa14a57dc9da3e47
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 15:57:48 -0700

    change

--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -14,7 +14,7 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <link rel="stylesheet" href="shop.css">
 </head>
 
@@ -193,7 +193,7 @@
                   <tfoot>
                      <tr>
                         <td>
-                           <a href="#" class="btn btn-primary">Continue Shopping</a>
+                           <a href="browse_shop.php" class="btn btn-primary">Continue Shopping</a>
                            <a href="#" class="btn btn-warning">Update Cart</a>
                         </td>
                         <td colspan="2" class="hidden-xs"></td>
