7c97051cd4073cb2baf9140f5713bf708d9b9a37
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 14:14:03 -0700

    change

--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -58,10 +58,10 @@
                         <td data-th="Quantity">
                            <input type="number" class="form-control text-center" value="1">
                         </td>
-                        <td data-th="Subtotal" class="text-center">1.99</td>
+                        <td data-th="Subtotal" class="text-center">200</td>
                         <td class="actions" data-th="">
-                           <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-                           <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+                           <button class="btn btn-info btn-sm"><i class="glyphicon glyphicon-refresh"></i></button>
+                           <button class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>
                         </td>
                      </tr>
                   </tbody>
@@ -72,7 +72,7 @@
                      <tr>
                         <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
                         <td colspan="2" class="hidden-xs"></td>
-                        <td class="hidden-xs text-center"><strong>Total <img src="pokedollar.png" alt="pokedollar" class="dollar-white">1.99</strong></td>
+                        <td class="hidden-xs text-center"><strong>Total <img src="pokedollar.png" alt="pokedollar" class="dollar-white">200</strong></td>
                         <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
                      </tr>
                   </tfoot>
