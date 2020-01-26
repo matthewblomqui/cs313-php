fed35e7f979faabeed8f7141aabc9634eb25e4d1
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-25 21:44:03 -0700

    change

--- web/week03/shopping_cart/browse_shop.php
+++ web/week03/shopping_cart/browse_shop.php
@@ -19,9 +19,11 @@
             <img src="pokemart_logo_small.jpg" alt="Logo" style="width:40px;">
          </a>
          <h3 class="text-light">Pokemart Shop</h3>
-         <a class="" href="view_cart.php">
-            <img src="cart.png" alt="Logo" style="background: white; border-radius: 50%;width:60px;">
-         </a>
+         <div class="collapse navbar-collapse" id="navb">
+            <a class="" href="view_cart.php">
+               <img src="cart.png" alt="Logo" style="background: white; border-radius: 50%;width:60px;">
+            </a>
+         </div>
       </nav>
 
       <div class="row">
