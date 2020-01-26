d8584710c13fddf061b4ccb693fc16be3690f23f
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 14:18:56 -0700

    change

--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -73,7 +73,7 @@
                      <tr>
                         <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
                         <td colspan="2" class="hidden-xs"></td>
-                        <td class="hidden-xs text-center"><strong>Total <img src="pokedollar.png" alt="pokedollar" class="dollar-white">200</strong></td>
+                        <td class="hidden-xs text-center"><strong>Total <img src="pokedollar.png" alt="pokedollar" class="dollar-white"> 200</strong></td>
                         <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
                      </tr>
                   </tfoot>
