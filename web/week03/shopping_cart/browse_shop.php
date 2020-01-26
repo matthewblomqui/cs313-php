800d95260ebbcd79f3d55399df271aea21cba570
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 12:45:02 -0700

    change

--- web/week03/shopping_cart/browse_shop.php
+++ web/week03/shopping_cart/browse_shop.php
@@ -61,7 +61,7 @@
                   <img src="pokeball.png" alt="pokeball" class="col-1 bg-light size">
                   <div class="col-1 bg-light">
                      <img src="pokedollar.png" alt="pokedollar" class="dollar">
-                     <p class="dollar">200</p>
+                     <b class="dollar">200</b>
                   </div>
                   <div class="col align-middle bg-light">A device for catching wild Pokémon. It's thrown like a ball at a Pokémon, comfortably encapsulating its target.</div>
                   <div class="algin-middle bg-light">Qty: 
