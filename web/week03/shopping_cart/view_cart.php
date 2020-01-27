36f06a43493c725e15ac5dd831675cdfb3bbf422
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 22:22:49 -0700

    change

--- web/week03/shopping_cart/view_cart.php
+++ web/week03/shopping_cart/view_cart.php
@@ -21,7 +21,7 @@
    <script>
       function tally() {
          var total = document.getElementById("total");
-         alert(document.getElementById("datapb").value);
+         alert("here");
          //var ball1 = document.getElementById("datapb").value;
          /*
          var ball2 = (document.getElementById("datagb").value == null ? 0 : document.getElementById("datagb").value);
