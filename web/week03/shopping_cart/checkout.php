f383f8c052d387d9f33bd2c77106f104222ad102
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 23:23:28 -0700

    change

--- web/week03/shopping_cart/checkout.php
+++ web/week03/shopping_cart/checkout.php
@@ -3,10 +3,14 @@
    <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title></title>
-      <meta name="description" content="">
+      <title>Pokemart Checkout</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="shop.css">
    </head>
    <body>
       <?php include 'nav.php';?>
