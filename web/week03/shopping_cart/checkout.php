989342ae42dddbaf5e51d33ecfb110004a5abf83
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-26 23:21:37 -0700

    change

--- web/week03/shopping_cart/checkout.php
+++ web/week03/shopping_cart/checkout.php
@@ -1,1 +1,18 @@
-<?php session_start(); print_r($_SESSION); ?>
+<?php session_start();?>
+<!DOCTYPE html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title></title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="">
+   </head>
+   <body>
+      <?php include 'nav.php';?>
+
+      <div class="col pt-3 pokeDark text-white"><?php print_r($_SESSION); ?></div>
+      
+      <?php include '../../footer.php';?>
+   </body>
+</html>
