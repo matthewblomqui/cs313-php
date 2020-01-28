15af63c7e7e969740dad44354699b0d4b6631c18
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-27 17:12:44 -0700

    change

--- web/header.php
+++ web/header.php
@@ -2,17 +2,17 @@
 echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link" href="https://matthewblomqui.herokuapp.com/index.php">Home</a>
             </li>
         </ul>
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="intro.php">Intro</a>
+                <a class="nav-link" href="https://matthewblomqui.herokuapp.com/intro.php">Intro</a>
             </li>
         </ul>
         <ul class="navbar-nav">
             <li class="nav-item dropdown">
-                <a class="nav-link" href="assignments.php">Assignments</a>
+                <a class="nav-link" href="https://matthewblomqui.herokuapp.com/assignments.php">Assignments</a>
             </li>
         </ul>
         <ul class="navbar-nav">
