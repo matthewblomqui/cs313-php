6d4ddb633f927737954482495033dd4b6241c603
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-01-30 21:26:35 -0700

    added database tables for main project

---
+++ db/myDb.sql
@@ -0,0 +1,27 @@
+CREATE TABLE pokemon
+(  id       SERIAL      NOT NULL PRIMARY KEY
+,  name     VARCHAR(15) NOT NULL
+,  type     VARCHAR(10) NOT NULL
+,  sub_type VARCHAR(10) NOT NULL
+,  ability  VARCHAR(15) NOT NULL
+,  nature   VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE moves
+(  id          SERIAL NOT NULL PRIMARY KEY
+,  move_name   VARCHAR(15)
+,  move_type   VARCHAR(10)
+);
+
+CREATE TABLE moveset
+(  id          SERIAL NOT NULL PRIMARY KEY
+,  pokemon_id  INT NOT NULL REFERENCES pokemon(id)
+,  move_id     INT REFERENCES moves(id)
+);
+
+CREATE TABLE team
+(  id          SERIAL NOT NULL PRIMARY KEY
+,  team_id     INT
+,  pokemon_id  INT REFERENCES pokemon(id)
+,  team_name   VARCHAR(20)
+);
