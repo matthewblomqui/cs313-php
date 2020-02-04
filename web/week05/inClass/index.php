0f300c3082931ed6ce730f05fa566388988df824
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-04 15:55:00 -0700

    inClass

--- web/week05/inClass/index.php
+++ web/week05/inClass/index.php
@@ -1,7 +1,6 @@
 <?php
    require "dbConnect.php";
-   //$db = get_db();
-   $db = get_db(); // calling this saves the database info in variable $db
+   $db = get_db();
 
    $family_members = $db->prepare("SELECT * FROM w5_family_members");
    $family_members->execute();
@@ -11,7 +10,13 @@
       $first_name = $fRow["first_name"];
       $last_name = $fRow["last_name"];
       $relationship_id = $fRow["relationship_id"];
+      $relation_name = $db->prepare("SELECT description FROM w5_relationships WHERE id = $relationship_id");
+      $relation_name->execute();
+      while($rRow = $relation_name->fetch(PDO::FETCH_ASSOC))
+      {
+         $relation_name = $rRow["description"];
+      }
 
-      echo "<p>$first_name $last_name is my $relationship_id</p>";
+      echo "<p>$first_name $last_name is my $relationship_name ($relationship_id)</p>";
    }
 ?>
