9b0b6950b7b2fc9a4b93e2f7b3f9c29aadee6d5d
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-04 12:04:05 -0700

    Added queries

--- web/week05/reading.php
+++ web/week05/reading.php
@@ -21,3 +21,22 @@ catch (PDOException $ex)
   die();
 }
 ?>
+
+<?php
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+
+/************SAME AS************/
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+/************SAME AS************/
+$statement = $db->query('SELECT username, password FROM note_user');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
