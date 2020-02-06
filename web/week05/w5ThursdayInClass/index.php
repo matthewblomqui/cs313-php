1ba96a4e7074cafccaaf41a0a56618b0e7938ec2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-06 16:33:03 -0700

    thursday

--- web/week05/w5ThursdayInClass/index.php
+++ web/week05/w5ThursdayInClass/index.php
@@ -1,3 +1,14 @@
-<?php 
+<?php
+   require "../inClass/dbConnect.php";
+   $db = get_db();
 
+   $images = $db->prepare("SELECT name, image FROM w5_event");
+   $images->execute();
+
+   while ($row = $images->fetch(PDO::FETCH_ASSOC))
+   {
+      $image = $row["image"];
+      $name = $row["name"];
+      echo "<h3>$name</h3><img src=\"$image\" alt=\"$name\"><br>";
+   }
 ?>
--- web/week05/w5ThursdayInClass/query.sql
+++ web/week05/w5ThursdayInClass/query.sql
@@ -1,29 +1,55 @@
 \echo '2a. How many events are there?'
----------------- Your code here ----------------
+SELECT COUNT(*) FROM w5_event;
 
 -- \echo '2b. How many participants are there?'
----------------- Your code here ----------------
+SELECT COUNT(*) FROM w5_participant;
 
 -- \echo '3a. What is the third event when sorted alphabetically (by name)? '
----------------- Your code here ----------------
+SELECT name FROM w5_event
+ORDER BY name
+LIMIT 1 OFFSET 2;
 
 -- \echo '3b. What is the third event when sorted by ID? '
----------------- Your code here ----------------
+SELECT id, name FROM w5_event
+ORDER BY id
+LIMIT 1 OFFSET 2;
 
 -- \echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
----------------- Your code here ----------------
+SELECT DISTINCT p.name FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE e.name = 'Toughman Utah'
+ORDER BY p.name;
 
 -- \echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
----------------- Your code here ----------------
+SELECT DISTINCT p.name, p.id FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE e.name = 'Kauai Marathon'
+ORDER BY p.id;
 
 -- \echo '5a. How many competitions has ''Black Panther'' competed in?'
----------------- Your code here ----------------
+SELECT COUNT(e.name) FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE p.name = 'Black Panther';
 
 -- \echo '5b. How can you verify that your count is correct?'  
----------------- Your code here ----------------
+SELECT id, name from w5_participant
+WHERE name = 'Black Panther';
+
+SELECT ep.id, ep.event_id, ep.participant_id FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE p.name = 'Black Panther';
 
 -- \echo '5c. Who has competed in the most competitions? How many have they competed in?'
----------------- Your code here ----------------
+SELECT p.name, COUNT(*) AS c FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+GROUP BY p.name
+ORDER BY c DESC
+LIMIT 1 OFFSET 0;
 
 -- \echo '5d. Who has competed in the least competitions? How many have they competed in?'
 ---------------- Your code here ----------------
@@ -45,3 +71,5 @@
 
 -- \echo '6d. List all competitors that competed in the same event at least once '
 ---------------- Your code here ----------------
+
+select image from w5_event;
