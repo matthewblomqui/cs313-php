3513ad136ff1babc2ec3075ddf3bf89837eeb5a1
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-06 11:56:58 -0700

    index change

--- web/week05/Prove/index.php
+++ web/week05/Prove/index.php
@@ -19,19 +19,19 @@
    </head>
    <body class="bp-light">
 
-      <div class="text-center pt-3">
-         <h2>Team Builder</h2>
+      <div class="text-center pt-3 bg-secondary">
+         <h2>Pokemon Battle Simulator</h2>
       </div>
 
       <hr>
 
       <div class="container p-3">
          <div class="row">
-            <div class="col-2">
+            <div class="col-2 bg-primary">
             </div>
             <div class="col-8">
             </div>
-            <div class="col-2">
+            <div class="col-2 bg-primary">
             </div>
          </div>
       </div>
