70e7e1c55f0dc49f74c824e3f7732aca562201a2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 16:49:23 -0700

    'c'

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -23,22 +23,15 @@
          <div class="col-8 text-center">
             
             <?php
-               $curl = curl_init();
-               $auth_data = array(
-                  'fields'    => 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;'
-                  'user-key' 	=> '56b3ac350a8ebcdd30e790eeced588bd',
-                  'Accept'   => 'application/json'
-               );
-               curl_setopt($curl, CURLOPT_POST, 1);
-               curl_setopt($curl, CURLOPT_POSTFIELDS, $auth_data);
-               curl_setopt($curl, CURLOPT_URL, 'https://api-v3.igdb.com/games/?user-key=56b3ac350a8ebcdd30e790eeced588bd');
-               curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-               curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-               $result = curl_exec($curl);
-               if(!$result){die("Connection Failure");}
-               curl_close($curl);
-               echo $result;
-
+               $ch = curl_init();
+               curl_setopt($ch, CURLOPT_URL, "http://www.myapi.com/api.php?format=json&action=subscribe&email=" . $email_to_subscribe);
+               curl_setopt($ch, CURLOPT_POST, 1);// set post data to true
+               curl_setopt($ch, CURLOPT_POSTFIELDS,"user-key=56b3ac350a8ebcdd30e790eeced588bd");   // post data
+               curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+               $json = curl_exec($ch);
+               curl_close ($ch);
+               $obj = json_decode($json);
+               print_r($obj);
 
                //$response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
                //$response = file_get_contents("https://api-v3.igdb.com/games/");
