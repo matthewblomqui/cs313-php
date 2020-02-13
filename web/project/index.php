a7e1cf83ef6e1ef76d25af1b75b982c35dcb4c18
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 15:38:54 -0700

    Added project file

---
+++ web/project/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <title>Pokemon Battle Simulator</title>
+      <link rel="stylesheet" href="prove.css">
+   </head>
+   <body>
+
+      <div class="row pt-3 pb-3 pokeDark text-warning">
+         <h2 class="text-center col">Pokemon Battle Simulator</h2>
+      </div>
+
+      <div class="row pokeLight">
+         <div class="col back">
+         </div>
+         <div class="col-8 text-center">
+            
+            <br>
+            <?php
+               require "dbConnect.php";
+               $db = get_db();
+
+               $pokemon = $db->prepare("SELECT * FROM pokemon");
+               $pokemon->execute();
+
+               while ($row = $pokemon->fetch(PDO::FETCH_ASSOC))
+               {
+                  $name = $row["name"];
+                  $type = $row["type"];
+                  $sub_type = $row["sub_type"];
+                  $ability = $row["ability"];
+                  $nature = $row["nature"];
+
+                  echo "<h3><b>Name:</b> $name <b>Type:</b> $type $sub_type <b>Ability:</b> $ability <b>Nature:</b> $nature</h3>";
+               }
+            ?>
+            <a href="teamBuilder.php" class="btn btn-primary">Team Builder</a>
+
+         </div>
+         <div class="col back">
+         </div>
+      </div>
+      
+      <?php include '../../footer.php';?>
+   </body>
+</html>
---
+++ web/project/teamBuilder.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <title>Pokemon Battle Simulator</title>
+      <link rel="stylesheet" href="prove.css">
+   </head>
+   <body>
+
+      <div class="row pt-3 pb-3 pokeDark text-warning">
+         <h2 class="text-center col">Team Builder</h2>
+      </div>
+
+      <div class="row pokeLight">
+         <div class="col back">
+         </div>
+         <div class="col-8 text-center">
+            
+            <h1>Here will be where the user creates a team and sends it to the database</h1>
+
+         </div>
+         <div class="col back">
+         </div>
+      </div>
+      
+      <?php include '../../footer.php';?>
+   </body>
+</html>
