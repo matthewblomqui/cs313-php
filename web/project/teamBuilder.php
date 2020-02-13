bba5f5bf8c1fab25bb8f508985fc0a5b70cf1cc4
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 16:45:53 -0700

    PokeApi request added

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -25,7 +25,9 @@
             <?php
                $curl = curl_init();
                $auth_data = array(
-                  'user-key' 	=> '56b3ac350a8ebcdd30e790eeced588bd'
+                  'fields'    => 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;'
+                  'user-key' 	=> '56b3ac350a8ebcdd30e790eeced588bd',
+                  'Accept'   => 'application/json'
                );
                curl_setopt($curl, CURLOPT_POST, 1);
                curl_setopt($curl, CURLOPT_POSTFIELDS, $auth_data);
