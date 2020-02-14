204f6220fc6c51f8603dc9fcadb519ca0fedd864
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 23:50:18 -0700

    'c'

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -35,7 +35,7 @@
          <div class="col-8 text-center">
             <div class="container">
                <div class="row">
-               <a class="btn-floating btn-dark"><i class="glyphicon glyphicon-plus"></i></a>
+               <a class="btn-floating btn-dark"><i class="fas fa-leaf"></i></a>
                </div>
                <br>
                <input class="form-control" id="search" type="text" placeholder="Search for Pokemon">
