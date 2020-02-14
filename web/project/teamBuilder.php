4437812bdc8f22b5381fbec3bbb382ccd2033678
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 22:37:00 -0700

    'c'

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -25,7 +25,7 @@
             <?php
                $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
                $response = json_decode($response, true);
-               echo "$response <br>"
+               echo "$response[0] <br>"
                //print_r($response);
             ?>
 
