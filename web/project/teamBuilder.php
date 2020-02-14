dafedbb7171311c369f11369686fd918c6c5a1de
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-13 22:58:25 -0700

    'c'

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -27,7 +27,7 @@
                //var_dump($response);
                $obj = json_decode($response, true);
                //var_dump($obj[0]);
-               var_dump($obj);
+               var_dump($obj['results']);
                //print_r($response);
             ?>
 
