20dfce415420c1ac71b7a6dd236f5c41b1a7ad06
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-14 11:44:22 -0700

    'c'

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -60,7 +60,7 @@
                         $url = $url['sprites']['front_default'];
 
 
-                        echo "<li class=\"list-group-item\">$url</li>";
+                        echo "<li class=\"list-group-item\"><img src=\"$url\" $str</li>";
                      }
                      //print_r($response);
                   ?>
