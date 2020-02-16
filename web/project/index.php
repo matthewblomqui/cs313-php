e7cd1cdb206d3beeed823b414cf97c7b127f3591
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-16 15:40:22 -0700

    'c'

--- web/project/index.php
+++ web/project/index.php
@@ -17,7 +17,7 @@
          <h2 class="text-center col">Pokemon Battle Simulator</h2>
       </div>
 
-      <div class="pokeLight">
+      <div class="pokeLight h-100">
          <div class="col back">
          </div>
          <div class="col-8 text-center">
