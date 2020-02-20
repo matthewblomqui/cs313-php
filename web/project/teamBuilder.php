f40c99cd96aafae772dd5e1079499cce42568ad2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-20 13:40:42 -0700

    changes

--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -59,7 +59,7 @@
                         $str = $obj['results'][$i]['name'];
                         $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/".($i+1).".png";
 
-                        echo "<div id\"test$i\" class=\"btn-group-vertical btn-group-toggle\" data-toggle=\"buttons\"><li class=\"btn btn-light\"><img style=\"width:48px; height=100%;\" src=\"$url\">$str</li></div>";
+                        echo "<div class=\"btn-group-vertical btn-group-toggle\" data-toggle=\"buttons\"><li id\"test$i\" class=\"btn btn-light\"><img style=\"width:48px; height=100%;\" src=\"$url\">$str</li></div>";
                      }
                   ?>
                </ul>
