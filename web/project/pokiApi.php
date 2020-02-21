a68fa9013b7767f8c0a409a636f4b29708de4dbb
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-20 18:58:05 -0700

    changes

---
+++ web/project/pokiApi.php
@@ -0,0 +1,16 @@
+<?php
+   $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
+   //var_dump($response);
+   $obj = json_decode($response, true);
+   //var_dump($obj[results][0]);
+   for ($i=0; $i < 10; $i++) { //801
+      $str = $obj['results'][$i]['name'];
+      $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/".($i+1).".png";
+
+      echo "<div class=\"btn-group-vertical btn-group-toggle\" data-toggle=\"buttons\">".
+               "<li class=\"btn btn-light\">".
+                  "<img style=\"width:48px; height=100%;\" src=\"$url\">$str".
+               "</li>".
+            "</div>";
+   }
+?>
--- web/project/teamBuilder.php
+++ web/project/teamBuilder.php
@@ -37,18 +37,7 @@
                <input class="form-control" id="search" type="text" placeholder="Search for Pokemon">
                <br>
                <ul class="list-group" id="list">
-                  <?php
-                     $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
-                     //var_dump($response);
-                     $obj = json_decode($response, true);
-                     //var_dump($obj[results][0]);
-                     for ($i=0; $i < 10; $i++) {
-                        $str = $obj['results'][$i]['name'];
-                        $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/".($i+1).".png";
-
-                        echo "<div class=\"btn-group-vertical btn-group-toggle\" data-toggle=\"buttons\"><li class=\"btn btn-light\"><img style=\"width:48px; height=100%;\" src=\"$url\">$str</li></div>";
-                     }
-                  ?>
+                  <?php include 'pokiApi.php';?>
                </ul>
                <br><br><br><br><br><br>
             </div>
