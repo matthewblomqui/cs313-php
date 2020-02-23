f407885821b769c1c44a25d18c25e00c89d9224a
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-23 15:57:02 -0700

    changes

--- web/project/insert.php
+++ web/project/insert.php
@@ -2,11 +2,11 @@
 
 $pokemon = array_map('str_getcsv', file('pokemon.csv'));
 $types = array_map('str_getcsv', file('types.csv'));
-$map = array("1"=>"normal","2"=>"fighting","3"=>"flying","4"=>"poison",
-             "5"=>"ground","6"=>"rock","7"=>"bug","8"=>"ghost",
-             "9"=>"steel","10"=>"fire","11"=>"water","12"=>"grass",
-             "13"=>"electric","14"=>"psychic","15"=>"ice","16"=>"dragon",
-             "17"=>"dark","18"=>"fairy");
+$map = array(1=>"normal",2=>"fighting",3=>"flying",4=>"poison",
+             5=>"ground",6=>"rock",7=>"bug",8=>"ghost",
+             9=>"steel",10=>"fire",11=>"water",12=>"grass",
+             13=>"electric",14=>"psychic",15=>"ice",16=>"dragon",
+             17=>"dark",18=>"fairy");
 
 
 $index = 1;
