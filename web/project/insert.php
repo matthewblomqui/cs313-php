831d6f037d3829387808b4721d5819ad006e2f75
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-23 15:53:29 -0700

    changes

--- web/project/insert.php
+++ web/project/insert.php
@@ -1,16 +1,31 @@
 <?php
 
-$csv = array_map('str_getcsv', file('pokemon.csv'));
+$pokemon = array_map('str_getcsv', file('pokemon.csv'));
+$types = array_map('str_getcsv', file('types.csv'));
+$map = array("1"=>"normal","2"=>"fighting","3"=>"flying","4"=>"poison",
+             "5"=>"ground","6"=>"rock","7"=>"bug","8"=>"ghost",
+             "9"=>"steel","10"=>"fire","11"=>"water","12"=>"grass",
+             "13"=>"electric","14"=>"psychic","15"=>"ice","16"=>"dragon",
+             "17"=>"dark","18"=>"fairy");
 
 
-
-for ($i=0; $i < 807; $i++) { 
+$index = 1;
+for ($i=0; $i < 808; $i++) { 
    # code...
 
-   $id = $csv[$i][0];
-   $name = $csv[$i][1];
+   $id = $pokemon[$i][0];
+   $name = $pokemon[$i][1];
    
    echo $id.' '.$name.'<br>';
+   $num = $types[$index][0];
+   while($num == $i)
+   {
+      $t = $types[$index][1];
+      echo $num.' '.$t.'<br>';
+      $index++;
+      $num = $types[$index][0];
+   }
+   
 }
 
 ?>
