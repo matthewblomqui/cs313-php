8da6732fd806483686279ae9f64d918ffa1bff57
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-22 23:10:52 -0700

    changes

--- web/project/signUp.php
+++ web/project/signUp.php
@@ -15,6 +15,9 @@
       
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
       <title>Login</title>
       <link rel="stylesheet" href="prove.css">
    </head>
@@ -32,7 +35,7 @@
          <div class="col back">
          </div>
          <div class="col-8 text-center">
-            <div class="alert alert-warning alert-dismissible">
+            <div class="alert alert-danger alert-dismissible">
                <button type="button" class="close" data-dismiss="alert">&times;</button>
                <strong>Warning!</strong> That Username has already been taken!
             </div>
