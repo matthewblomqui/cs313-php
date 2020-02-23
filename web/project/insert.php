5f6bcb97ba966b80de4948758c6be22e3369e989
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-23 15:24:27 -0700

    changes

--- web/project/insert.php
+++ web/project/insert.php
@@ -3,7 +3,7 @@
 $csv = array_map('str_getcsv', file('pokemon.csv'));
 
 //var_dump($csv);
-for ($i=0; $csv[i][7] == 1; $i++) { 
+for ($i=0; $i < 807; $i++) { 
    # code...
    echo $csv[i][0].' '.$csv[i][1]."<br>";
 }
