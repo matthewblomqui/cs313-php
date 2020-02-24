47a77b78775b0d76560fdddfec8a34143122cefe
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-23 21:05:01 -0700

    changes

--- web/project/script.js
+++ web/project/script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-   $("#search").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#list li").filter(function() {
-         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-   });
-   $('.mdb-select').materialSelect({
-   });
+// $(document).ready(function(){
+//    $("#search").on("keyup", function() {
+//       var value = $(this).val().toLowerCase();
+//       $("#list li").filter(function() {
+//          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//       });
+//    });
+// });
+
+$('.mdb-select').materialSelect({
 });
