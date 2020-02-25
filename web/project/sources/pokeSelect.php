5d18e0acd25004779d5e462172923f71115d59ad
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-24 20:52:09 -0700

    changes

--- web/project/sources/pokeSelect.php
+++ web/project/sources/pokeSelect.php
@@ -1,15 +1,16 @@
 <?php
-   $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
-   //var_dump($response);
-   $obj = json_decode($response, true);
-   //var_dump($obj[results][0]);
-   for ($i=0; $i < 10; $i++) { //801
-      $j = $i + 1;
-      $str = $obj['results'][$i]['name'];
-      $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$j.png";
+   // $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
+   // //var_dump($response);
+   // $obj = json_decode($response, true);
+   // //var_dump($obj[results][0]);
+   // for ($i=0; $i < 10; $i++) { //801
+   //    $j = $i + 1;
+   //    $str = $obj['results'][$i]['name'];
+   //    $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$j.png";
 
-      echo "<option value=\"$str\"><img style=\"width:52px; height=100%;\" src=\"$url\">".
-            "<span class=\"pr-3\">$str</span>".
-            "<span class=\"type normal\"></option>";
-   }
+   //    echo "<option value=\"$str\"><img style=\"width:52px; height=100%;\" src=\"$url\">".
+   //          "<span class=\"pr-3\">$str</span>".
+   //          "<span class=\"type normal\"></option>";
+   // }
+   echo "<option value=\"Lucario\">Lucario</option>"
 ?>
