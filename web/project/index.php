2f90bbf04e3aa726427b1d39804f7c39de955bd6
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 14:57:23 -0700

    changes

--- web/project/index.php
+++ web/project/index.php
@@ -38,7 +38,40 @@
             
             <br>
             <h4>Welcome <?php echo $username.', Here are your teams';?></h4>
+            <form action="" method="POST">
+
+            <div class="container">
+               <h2>Hover Rows</h2>
+               <p>The .table-hover class enables a hover state (grey background on mouse over) on table rows:</p>            
+               <table class="table table-hover">
+                  <thead>
+                     <tr>
+                     <th>Firstname</th>
+                     <th>Lastname</th>
+                     <th>Email</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                     <td>John</td>
+                     <td>Doe</td>
+                     <td>john@example.com</td>
+                     </tr>
+                     <tr>
+                     <td>Mary</td>
+                     <td>Moe</td>
+                     <td>mary@example.com</td>
+                     </tr>
+                     <tr>
+                     <td>July</td>
+                     <td>Dooley</td>
+                     <td>july@example.com</td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
             <?php
+               
                $team = $db->prepare("SELECT * FROM team WHERE user_id = $id");
                $team->execute();
 
@@ -55,6 +88,7 @@
                   echo $p1.'<br>';
                }
             ?>
+            </form>
             <!-- <?php
                $pokemon = $db->prepare("SELECT * FROM pokemon");
                $pokemon->execute();
