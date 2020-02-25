99eb9b9b32ed693589bde06bf70667334519ac17
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 12:14:34 -0700

    changes

--- web/project/post_team.php
+++ web/project/post_team.php
@@ -12,16 +12,16 @@
    $pokemon_5 = $_POST['pokemon_5'];
    $pokemon_6 = $_POST['pokemon_6'];
 
-   $insert = $db->prepare("INSERT INTO team (name, user_id, pokemon_1, pokemon_2, pokemon_3, pokemon_4, pokemon_5, pokemon_6) VALUES (:name, :id, :p1, :p2, :p3, :p4, :p5, :p6)");
-   $insert->bindValue(':name',$team);
-   $insert->bindValue(':id',$id);
-   $insert->bindValue(':p1',$pokemon_1);
-   $insert->bindValue(':p2',$pokemon_2);
-   $insert->bindValue(':p3',$pokemon_3);
-   $insert->bindValue(':p4',$pokemon_4);
-   $insert->bindValue(':p5',$pokemon_5);
-   $insert->bindValue(':p6',$pokemon_6);
-   $insert->execute();
+   $insertStatement = $db->prepare("INSERT INTO team (name, user_id, pokemon_1, pokemon_2, pokemon_3, pokemon_4, pokemon_5, pokemon_6) VALUES (:team_name, :id, :p1, :p2, :p3, :p4, :p5, :p6)");
+   $insertStatement->bindValue(':team_name',$team);
+   $insertStatement->bindValue(':id',$id);
+   $insertStatement->bindValue(':p1',$pokemon_1);
+   $insertStatement->bindValue(':p2',$pokemon_2);
+   $insertStatement->bindValue(':p3',$pokemon_3);
+   $insertStatement->bindValue(':p4',$pokemon_4);
+   $insertStatement->bindValue(':p5',$pokemon_5);
+   $insertStatement->bindValue(':p6',$pokemon_6);
+   $insertStatement->execute();
 
    header('Location: ' . $url);
    die();
