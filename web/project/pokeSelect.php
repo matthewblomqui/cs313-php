92a07f12013ba6ab5999c3f74c1637270015a51c
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-24 21:15:43 -0700

    changes

--- web/project/pokeSelect.php
+++ web/project/pokeSelect.php
@@ -1,15 +1,11 @@
 <?php
    $response = file_get_contents("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000");
-   //var_dump($response);
    $obj = json_decode($response, true);
-   //var_dump($obj[results][0]);
-   for ($i=0; $i < 10; $i++) { //801
-      $j = $i + 1;
-      $str = $obj['results'][$i]['name'];
-      $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$j.png";
 
-      echo "<option value=\"$str\"><img style=\"width:52px; height=100%;\" src=\"$url\">".
-            "<span class=\"pr-3\">$str</span>".
-            "<span class=\"type normal\"></option>";
+   for ($i=0; $i < 807; $i++) { //801
+      $j = $i + 1;
+      $name = $obj['results'][$i]['name'];
+
+      echo "<option value=\"$name\">$name</option>";
    }
 ?>
