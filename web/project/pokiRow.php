8f67ba18a6ffacac705860ffc154c026ff6d3ad9
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 16:04:45 -0700

    changes

---
+++ web/project/pokiRow.php
@@ -0,0 +1,36 @@
+<?php
+   session_start();
+   require 'dbConnect.php';
+   $db = get_db();
+   $id = $_SESSION['userId'];
+
+   $teams = $db->prepare("SELECT * FROM team WHERE user_id = $id");
+   $teams->execute();
+
+   while ($row = $teams->fetch(PDO::FETCH_ASSOC))
+   {
+      $team_name = $row['team_name'];
+      $p1 = $row['pokemon_1'];
+      $p2 = $row['pokemon_2'];
+      $p3 = $row['pokemon_3'];
+      $p4 = $row['pokemon_4'];
+      $p5 = $row['pokemon_5'];
+      $p6 = $row['pokemon_6'];
+
+      echo "<td>".  
+               "<div class=\"custom-control custom-checkbox\">".
+                  "<input type=\"checkbox\" class=\"custom-control-input\" id=\"customCheck\" name=\"example1\">".
+                  "<label class=\"custom-control-label\" for=\"customCheck\"></label>".
+               "</div>".
+            "</td>";
+      
+      $pokemon = $db->prepare("SELECT * FROM pokemon WHERE pokedex = $p1");
+      $pokemon->execute();
+      if ($pRow = $pokemon->fetch(PDO::FETCH_ASSOC))
+      {
+         echo $pRow['name'];
+      }
+
+   }
+
+   ?>
