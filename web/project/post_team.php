1d4f3ddf723dca0fedd10d392cb4a5dad30587ef
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 13:07:35 -0700

    changes

--- web/project/post_team.php
+++ web/project/post_team.php
@@ -11,14 +11,6 @@
    $p4 = $_POST['pokemon_4'];
    $p5 = $_POST['pokemon_5'];
    $p6 = $_POST['pokemon_6'];
-   echo $id.'<br>';
-   echo $team.'<br>';
-   echo $p1.'<br>';
-   echo $p2.'<br>';
-   echo $p3.'<br>';
-   echo $p4.'<br>';
-   echo $p5.'<br>';
-   echo $p6.'<br>';
 
    try
    {
@@ -26,12 +18,12 @@
       $insertStatement = $db->prepare($query);
       $insertStatement->bindValue(':team',$team);
       $insertStatement->bindValue(':id',$id);
-      $insertStatement->bindValue(':p1',(int)$p1);
-      $insertStatement->bindValue(':p2',(int)$p2);
-      $insertStatement->bindValue(':p3',(int)$p3);
-      $insertStatement->bindValue(':p4',(int)$p4);
-      $insertStatement->bindValue(':p5',(int)$p5);
-      $insertStatement->bindValue(':p6',(int)$p6);
+      $insertStatement->bindValue(':p1',$p1);
+      $insertStatement->bindValue(':p2',$p2);
+      $insertStatement->bindValue(':p3',$p3);
+      $insertStatement->bindValue(':p4',$p4);
+      $insertStatement->bindValue(':p5',$p5);
+      $insertStatement->bindValue(':p6',$p6);
       $insertStatement->execute();
    }
    catch (Exception $ex)
@@ -39,8 +31,6 @@
       echo "Error with DB. Details: $ex";
       die();
    }
-   echo "Success?";
-
-   // header('Location: ' . $url);
-   // die();
+   header('Location: ' . $url);
+   die();
 ?>
