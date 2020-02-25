b7aac2cbb586122237ed5a5775724d0636adbdb7
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 12:53:29 -0700

    changes

--- web/project/post_team.php
+++ web/project/post_team.php
@@ -12,16 +12,24 @@
    $p5 = $_POST['pokemon_5'];
    $p6 = $_POST['pokemon_6'];
 
-   $query = "INSERT INTO team (team_name, user_id, pokemon_1, pokemon_2, pokemon_3, pokemon_4, pokemon_5, pokemon_6) VALUES (:team,:id,:p1,:p2,:p3,:p4,:p5,:p6)";
-   $insertStatement = $db->prepare($query);
-   $insertStatement->bindValue(':team',$team);
-   $insertStatement->bindValue(':id',$id);
-   $insertStatement->bindValue(':p1',$p1);
-   $insertStatement->bindValue(':p2',$p2);
-   $insertStatement->bindValue(':p3',$p3);
-   $insertStatement->bindValue(':p4',$p4);
-   $insertStatement->bindValue(':p5',$p5);
-   $insertStatement->bindValue(':p6',$p6);
+   try
+   {
+      $query = "INSERT INTO team (team_name, user_id, pokemon_1, pokemon_2, pokemon_3, pokemon_4, pokemon_5, pokemon_6) VALUES (:team,:id,:p1,:p2,:p3,:p4,:p5,:p6)";
+      $insertStatement = $db->prepare($query);
+      $insertStatement->bindValue(':team',$team);
+      $insertStatement->bindValue(':id',$id);
+      $insertStatement->bindValue(':p1',$p1);
+      $insertStatement->bindValue(':p2',$p2);
+      $insertStatement->bindValue(':p3',$p3);
+      $insertStatement->bindValue(':p4',$p4);
+      $insertStatement->bindValue(':p5',$p5);
+      $insertStatement->bindValue(':p6',$p6);
+   }
+   catch (Exception $ex)
+   {
+      echo "Error with DB. Details: $ex";
+      die();
+   }
    //$insertStatement->execute();
    echo "Success?";
 
