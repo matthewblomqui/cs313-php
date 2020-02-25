4dab07b0025102d2d16ee3c6949106d0266e5495
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 11:25:42 -0700

    changes

--- web/project/script.js
+++ web/project/script.js
@@ -71,30 +71,50 @@ $(document).ready(function(){
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_2").attr("src",str);
+      $("#name_2").text(dex[pokedex - 1][0]);
+      $("#type_2_1").removeClass();
+      $("#type_2_1").addClass("type");
+      $("#type_2_1").addClass(types[dex[pokedex - 1][1]]);
    });
    $("#p_3").change(function() {
       var pokedex = $("#p_3").val();
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_3").attr("src",str);
+      $("#name_3").text(dex[pokedex - 1][0]);
+      $("#type_3_1").removeClass();
+      $("#type_3_1").addClass("type");
+      $("#type_3_1").addClass(types[dex[pokedex - 1][1]]);
    });
    $("#p_4").change(function() {
       var pokedex = $("#p_4").val();
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_4").attr("src",str);
+      $("#name_4").text(dex[pokedex - 1][0]);
+      $("#type_4_1").removeClass();
+      $("#type_4_1").addClass("type");
+      $("#type_4_1").addClass(types[dex[pokedex - 1][1]]);
    });
    $("#p_5").change(function() {
       var pokedex = $("#p_5").val();
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_5").attr("src",str);
+      $("#name_5").text(dex[pokedex - 1][0]);
+      $("#type_5_1").removeClass();
+      $("#type_5_1").addClass("type");
+      $("#type_5_1").addClass(types[dex[pokedex - 1][1]]);
    });
    $("#p_6").change(function() {
       var pokedex = $("#p_6").val();
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_6").attr("src",str);
+      $("#name_6").text(dex[pokedex - 1][0]);
+      $("#type_6_1").removeClass();
+      $("#type_6_1").addClass("type");
+      $("#type_6_1").addClass(types[dex[pokedex - 1][1]]);
    });
 });
 
