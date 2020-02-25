2a46dc719589300cf5727ab9aea9da6faffb7a49
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 12:56:52 -0700

    changes

--- web/project/post_team.php
+++ web/project/post_team.php
@@ -18,12 +18,12 @@
       $insertStatement = $db->prepare($query);
       $insertStatement->bindValue(':team',$team);
       $insertStatement->bindValue(':id',$id);
-      $insertStatement->bindValue(':p1',$p1);
-      $insertStatement->bindValue(':p2',$p2);
-      $insertStatement->bindValue(':p3',$p3);
-      $insertStatement->bindValue(':p4',$p4);
-      $insertStatement->bindValue(':p5',$p5);
-      $insertStatement->bindValue(':p6',$p6);
+      $insertStatement->bindValue(':p1',(int)$p1);
+      $insertStatement->bindValue(':p2',(int)$p2);
+      $insertStatement->bindValue(':p3',(int)$p3);
+      $insertStatement->bindValue(':p4',(int)$p4);
+      $insertStatement->bindValue(':p5',(int)$p5);
+      $insertStatement->bindValue(':p6',(int)$p6);
       $insertStatement->execute();
    }
    catch (Exception $ex)
