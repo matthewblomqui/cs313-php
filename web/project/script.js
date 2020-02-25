012de81870c4f2c1fed7ca6364ca6fac048c9ae2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-24 22:18:50 -0700

    changes

--- web/project/script.js
+++ web/project/script.js
@@ -13,11 +13,40 @@ function teams() {
 
 $(document).ready(function(){
    $("#p_1").change(function() {
-      //alert($("#p_1").val());
       var pokedex = $("#p_1").val();
       var str = "";
       str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
       $("#preview_1").attr("src",str);
+   });
+   $("#p_2").change(function() {
+      var pokedex = $("#p_2").val();
+      var str = "";
+      str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
+      $("#preview_2").attr("src",str);
+   });
+   $("#p_3").change(function() {
+      var pokedex = $("#p_3").val();
+      var str = "";
+      str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
+      $("#preview_3").attr("src",str);
+   });
+   $("#p_4").change(function() {
+      var pokedex = $("#p_4").val();
+      var str = "";
+      str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
+      $("#preview_4").attr("src",str);
+   });
+   $("#p_5").change(function() {
+      var pokedex = $("#p_5").val();
+      var str = "";
+      str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
+      $("#preview_5").attr("src",str);
+   });
+   $("#p_6").change(function() {
+      var pokedex = $("#p_6").val();
+      var str = "";
+      str = str.concat("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",pokedex,".png");
+      $("#preview_6").attr("src",str);
    });
 });
 
