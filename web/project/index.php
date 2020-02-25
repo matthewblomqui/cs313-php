d78ff3d3e4a7bcb55864705c34e8788949c3af2c
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 16:54:26 -0700

    changes

--- web/project/index.php
+++ web/project/index.php
@@ -35,48 +35,7 @@
          <div class="col back"></div>
 
          <div class="col-8 text-center">
-            <!-- <br>
-            <h4>Welcome <?php echo $username.', Here are your teams';?></h4>
-            <form action="" method="POST">
-               <div class="container">          
-                  <table class="table table-hover">
-                     <thead>
-                        <tr>
-                        <th></th>
-                        <th>Team Name</th>
-                        <th>Pokemon 1</th>
-                        <th>Pokemon 2</th>
-                        <th>Pokemon 3</th>
-                        <th>Pokemon 4</th>
-                        <th>Pokemon 5</th>
-                        <th>Pokemon 6</th>
-                        </tr>
-                     </thead>
-                     <tbody>
-                        <tr>
-                        <td>  
-                           <div class="custom-control custom-checkbox">
-                              <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                              <label class="custom-control-label" for="customCheck"></label>
-                           </div>
-                        </td>
-                        <td>Team Boss</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png">Lucario</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png">Gengar</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png">Blaziken</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/802.png">Marshadow</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/282.png">Gardevoir</td>
-                        <td><img style="width:48px; height:auto;" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png">Venusaur</td>
-                        </tr>
-                        <?php //include 'pokiRow.php'; ?>
 
-                     </tbody>
-                  </table>
-               </div>
-            </form>
-            <a href="teamBuilder.php" class="btn btn-primary">Go to Team Builder</a>
-            <a href="" class="btn btn-danger">Remove Selected Teams</a>
-            <br><br><br><br><br><br> -->
          </div>
 
          <div class="col back"></div>
