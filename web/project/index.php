4d5ba6efabfdf17e86768f5d643647ad2dfa6274
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 15:04:16 -0700

    changes

--- web/project/index.php
+++ web/project/index.php
@@ -59,6 +59,7 @@
                      <td>  
                         <div class="custom-control custom-checkbox">
                            <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+                           <label class="custom-control-label" for="customCheck"></label>
                         </div>
                      </td>
                      <td>John</td>
