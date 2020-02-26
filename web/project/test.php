40b12f4ed8028e147701310e62ff2f147ff2f41e
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 17:59:12 -0700

    changes

--- web/project/test.php
+++ web/project/test.php
@@ -30,10 +30,13 @@
    $id = $_SESSION['userId'];
    
    // $db = get_db();
-   // $teams = $db->prepare("SELECT * FROM team WHERE user_id = $id");
-   // $teams->execute();
+   $teams = $db->prepare("SELECT * FROM team WHERE user_id = $id");
+   $teams->execute();
 
    // check box for first column
    echo '<td><div class="custom-control custom-checkbox"><input type="checkbox" class="custom-control-input" id="check1" name="example1"><label class="custom-control-label" for="check1"></label></td>';
-
+   if ($row = $teams->fetch(PDO::FETCH_ASSOC))
+   {
+      echo '<td>'.$row['team_name'].'</td>';
+   }
 ?>
