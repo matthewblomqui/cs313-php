aaad526b82c17baa17a094a1469d726ec83162de
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 22:41:28 -0700

    changes

--- web/project/remove.php
+++ web/project/remove.php
@@ -2,5 +2,11 @@
    foreach ($_POST as $key => $value) {
       # code...
       echo "here is the key:".$key;
+
+      // sql to delete a record
+      $sql = "DELETE FROM team WHERE id=$key";
+      $statement = $db->prepare($sql);
+      $statement->execute();
+
    }
 ?>
