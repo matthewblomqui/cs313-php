6f431eb44151046e5664ee64cd9bfcffd2f954df
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-02-25 17:56:38 -0700

    changes

--- web/project/test.php
+++ web/project/test.php
@@ -1,30 +1,28 @@
 <?php
-   function get_db() {
-      $db = NULL;
-      try
-      {
-         // Using environment variable to get URL: keyname 'DATABASE_URL'
-         $dbUrl = getenv('DATABASE_URL');
+   $db = NULL;
+   try
+   {
+      // Using environment variable to get URL: keyname 'DATABASE_URL'
+      $dbUrl = getenv('DATABASE_URL');
 
-         $dbOpts = parse_url($dbUrl);
+      $dbOpts = parse_url($dbUrl);
 
-         $dbHost = $dbOpts["host"];
-         $dbPort = $dbOpts["port"];
-         $dbUser = $dbOpts["user"];
-         $dbPassword = $dbOpts["pass"];
-         $dbName = ltrim($dbOpts["path"],'/');
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
 
-         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-      catch (PDOException $ex)
-      {
-         //echo 'Error!: ' . $ex->getMessage();
-         die();
-      }
-      return $db;
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    }
+   catch (PDOException $ex)
+   {
+      //echo 'Error!: ' . $ex->getMessage();
+      die();
+   }
+   return $db;
 ?>
 
 
